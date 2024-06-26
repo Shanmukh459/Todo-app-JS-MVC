c4a99b19d95796ab808e6342d900675a31bcc826
Shanmukh459 <saikrrish459@gmail.com>
2024-06-26 11:49:15 -0700

    created ui elements in the view and added styles

--- index.js
+++ index.js
@@ -30,7 +30,42 @@ class Model {
 
 class View {
   constructor() {
+    //root element
+    this.root = this.getElement("#root")
 
+    //app title
+    this.heading = this.createElement("h1")
+    this.heading.textContent = "Todos"
+
+    //the input form
+    this.form = this.createElement("form")
+
+    this.input = this.createElement("input")
+    this.input.type = "text"
+    this.input.placeholder = "Add todo"
+    this.input.name = 'todo'
+
+    this.submitButton = this.createElement("button")
+    this.submitButton.textContent = "Submit" 
+
+    //visual representation of todo list
+    this.todoList = this.createElement('ul', 'todo-list')
+
+    //append input and submit button to form
+    this.form.append(this.input, this.submitButton)
+
+    //append title, form and todolist to the app
+    this.app.append(this.title, this.form, this.todoList)
+
+  }
+  createElement(tag, className) {
+    const element = document.createElement(tag)
+    if(className) element.classList.add(className)
+    return element
+  }
+  getElement(selector) {
+    const element = document.querySelector(selector)
+    return element
   }
 }
 
