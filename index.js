11a0493bd08c514d22e8919d91ba8f33cfbfaf28
Shanmukh459 <saikrrish459@gmail.com>
2024-06-26 13:03:51 -0700

    binding model and controller to call onTodoChanged

--- index.js
+++ index.js
@@ -12,19 +12,27 @@ class Model {
       complete: false
     }
     this.todos.push(todo)
+    this.onTodoListChanged(this.todos)
   }
   editTodo(id, updatedText) {
     this.todos = this.todos.map(todo => 
       todo.id === id? {id: id, text: updatedText, complete: todo.complete} : todo
     )
+    this.onTodoListChanged(this.todos)
   }
   deleteTodo(id) {
     this.todos = this.todos.filter(todo => todo.id !== id)
+    this.onTodoListChanged(this.todos)
   }
   toggleTodo(id) {
     this.todos = this.todos.map(todo => 
       todo.id === id ? {id, text: todo.text, complete: !todo.complete} : todo
     )
+    this.onTodoListChanged(this.todos)
+  }
+
+  bindOnTodoListChanged(callback) {
+    this.onTodoListChanged = callback
   }
 }
 
@@ -158,10 +166,17 @@ class Controller {
     this.view = view
 
     //display initial todos
-    this.onTodoListChanges(this.model.todos)
+    this.onTodoListChanged(this.model.todos)
+
+    this.view.bindAddTodo(this.handleAddTodo)
+    this.view.bindDeleteTodo(this.handleDeleteTodo)
+    this.view.bindToggleTodo(this.handleToggleTodo)
+    // this.view.bindEditTodo(this.handleEditTodo)
+
+    this.model.bindOnTodoListChanged(this.onTodoListChanged)
   }
 
-  onTodoListChanges = (todos) => {
+  onTodoListChanged = (todos) => {
     this.view.displayTodos(todos)
   }
 
@@ -172,6 +187,8 @@ class Controller {
   handleEditTodo = (id, text) => this.model.editTodo(id, text)
 
   handleToggleTodo = (id) => this.model.toggleTodo(id)
+
+
 }
 
 const app = new Controller(new Model(), new View())
