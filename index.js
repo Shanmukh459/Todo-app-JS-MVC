61997cb7633e9262d3f5111880ec2dc4052540f3
Shanmukh459 <saikrrish459@gmail.com>
2024-06-26 11:28:40 -0700

    hooked up index.js and also added crud functionality to model class

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 class Model {
   constructor() {
-
+    this.todos = [
+      {id: 1, text: 'Run a marathon', complete: false},
+      {id: 2, text: 'Plant a garden', complete: false},
+    ]
+  }
+  addTodo(todoText) {
+    const todo = {
+      id: this.todos.length > 0 ? this.todos[this.todos.length-1].id+1 : 1,
+      text: todoText,
+      complete: false
+    }
+    this.todos.push(todo)
+  }
+  editTodo(id, updatedText) {
+    this.todos = this.todos.map(todo => 
+      todo.id === id? {id: id, text: updatedText, complete: todo.complete} : todo
+    )
+  }
+  deleteTodo(id) {
+    this.todos = this.todos.filter(todo => todo.id !== id)
+  }
+  toggleTodo(id) {
+    this.todos = this.todos.map(todo => 
+      todo.id === id ? {id, text: todo.text, complete: !todo.complete} : todo
+    )
   }
 }
 
